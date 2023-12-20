0a665004635189e97daa6a3dc7b52548e1de9690
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2023-12-20 12:47:47 +0200

    An Alternative To Lifting State Up

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,11 @@ function deriveActivePlayer(gameTurns) {
 }
 
 function App() {
+  const [players , setPlayers] = useState({
+    X: 'Player 1',
+    O: 'Player 2'
+  });
   const [gameTurns, setGameTurns] = useState([]);
-  // const [activePlayer, setActivePlayer] = useState('X');
 
   const activePlayer = deriveActivePlayer(gameTurns);
 
@@ -49,7 +52,7 @@ function App() {
       firstSquareSymbol === secondtSquareSymbol &&
       firstSquareSymbol === thirdquareSymbol
     ) {
-      winner = firstSquareSymbol;
+      winner = players[firstSquareSymbol];
     }
 
   }
@@ -73,12 +76,31 @@ function App() {
     setGameTurns([]);
   }
 
+  function handlePlayerNameChange(symbol, newName) {
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      };
+    });
+  }
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
-          <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === 'X'}
+            onChangeName={handlePlayerNameChange}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === 'O'}
+            onChangeName={handlePlayerNameChange}
+          />
         </ol>
         {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} />} {/*winner is true*/}
         <GameBoard onSelectSquare={handleSelecterSquare} board={gameBoard} />
